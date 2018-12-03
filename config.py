b0700d61a8b2f05e71f518c9ed786bd1382b30d8
TooT95 <32481709+TooT95@users.noreply.github.com>
2018-12-03 22:35:06 +0500

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ URL = os.environ.get('https://swgangs.herokuapp.com/736499313:AAEkYrlxvxRYQW2Wpp
 PORT = int(os.environ.get('PORT', '5000'))
 
 # telegram
-TOKEN = os.environ.get('736499313:AAEkYrlxvxRYQW2WppS-xXdeZpznM5KGTSY')
+TOKEN = os.environ.get('741015930:AAFyfoX9RxOFrzDjFmyv2iGhbCTtDAziQJQ')
