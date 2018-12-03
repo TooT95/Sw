3cfb7b0c77331dba3523a166a30ecf395c906e14
TooT95 <32481709+TooT95@users.noreply.github.com>
2018-12-03 22:36:10 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 # webhook
-URL = os.environ.get('https://swgangs.herokuapp.com/736499313:AAEkYrlxvxRYQW2WppS-xXdeZpznM5KGTSY')
+URL = os.environ.get('https://swgangs.herokuapp.com/741015930:AAFyfoX9RxOFrzDjFmyv2iGhbCTtDAziQJQ')
 PORT = int(os.environ.get('PORT', '5000'))
 
 # telegram
