8a317305edded136776f1dbb2e86f70d45e79c3a
TooT95 <32481709+TooT95@users.noreply.github.com>
2018-12-05 18:45:22 +0500

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# webhook
-URL = os.environ.get('https://swgangs.herokuapp.com/736499313:AAEkYrlxvxRYQW2WppS-xXdeZpznM5KGTSY')
-PORT = int(os.environ.get('PORT', '5000'))
-
-# telegram
-TOKEN = os.environ.get('736499313:AAEkYrlxvxRYQW2WppS-xXdeZpznM5KGTSY')
